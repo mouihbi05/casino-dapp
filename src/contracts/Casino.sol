c15fce20a013c1749b1abab93b1607eb45248ca9
Viraz <virajm72@gmail.com>
2019-12-07 11:35:59 +0530

    Test Casses Written

--- src/contracts/Casino.sol
+++ src/contracts/Casino.sol
@@ -10,7 +10,6 @@ contract Casino {
     uint public winnerNumber;
 
     address payable[] public winners;
-    uint public winnerCount;
     uint public winnerAmount;
     address payable[] public existingPlayers;
 
@@ -36,7 +35,6 @@ contract Casino {
         return false;
     }
     function bet(uint _num)  public payable  {
-
         require(_num >=1 && _num <=10);
         require(msg.value >= minBetPrice);
         require(!checkIfPlayerExists(msg.sender));
@@ -44,6 +42,9 @@ contract Casino {
         noOfBets ++;
         totalBetAmount += msg.value;
         existingPlayers.push(msg.sender); 
+        if(noOfBets == maxBets) {
+            generateWinner();
+        }
         
     }
 
@@ -56,15 +57,12 @@ contract Casino {
     function distributePrizes(uint _num) public payable {
          for(uint i = 0; i < existingPlayers.length; i++) {
              if(playerInfo[existingPlayers[i]].betNo == _num) {
-                 winnerCount ++;
                  winners.push(existingPlayers[i]);
              }
              delete playerInfo[existingPlayers[i]];
          }
-         existingPlayers.length = 0;
-         winnerAmount = totalBetAmount / winnerCount;
+         winnerAmount = totalBetAmount / winners.length;
          for (uint i = 0; i < winners.length; i++) {
-             
              winners[i].transfer(winnerAmount);
          }
     }
